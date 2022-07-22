6844338f5a2bd8449091bf1806f2ed1060ce941e
OscarMasterG7 <oscarmastergarcia@outlook.com>
2022-07-23 01:07:21 +0200

    Prueba lectura de Api sin API Keys aun.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const GIFS = ["https://media1.giphy.com/media/Oa9gZy19igIS5pWNVa/giphy.webp","https://media0.giphy.com/media/4jtYxHxBZSMeBNVg4z/giphy.webp"]
+
+
 function App() {
+  const [gifs, setGifs] = useState(GIFS)
+  
   return (
     <div className="App">
-      
+      <section className="App-content">
+        {
+          gifs.map(singleGif => <img src={singleGif}/> )
+        }
+      </section>
     </div>
   )
 }
